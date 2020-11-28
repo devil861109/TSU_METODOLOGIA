2166554398687a0c88f09ce4697b13b764d33fc5
Alfonso <devil861109@gmail.com>
2020-11-28 02:41:04 -0600

    cambios

---
+++ Tema 05 Fundamentos de la Programación/sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+	printf ("Tamanio de int: %d \n", sizeof(int));
+	printf ("Tamanio de double: %d \n", sizeof(double));
+	printf ("Tamanio de char: %d \n", sizeof(char));
+	
+	return 0;
+}
